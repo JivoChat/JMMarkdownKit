1553ec5dd0c796baff440b56f3fba9d7e7d8e306
Stan Potemkin <stan@potemkin.su>
2025-01-21 00:41:12 +0300

    Fix UIKit imports

--- JMMarkdownKit/JMMarkdownHrefElement.swift
+++ JMMarkdownKit/JMMarkdownHrefElement.swift
@@ -4,6 +4,7 @@
 //
 
 import Foundation
+import UIKit
 import JFMarkdownKit
 
 public final class JMMarkdownHrefElement: JMMarkdownClickableElement {
--- JMMarkdownKit/JMMarkdownImageElement.swift
+++ JMMarkdownKit/JMMarkdownImageElement.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 import JFMarkdownKit
 
 public final class JMMarkdownImageElement: CDMarkdownLinkElement {
